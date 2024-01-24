b06b43b88c75babed9eec6d20735a8540b14d362
GabrielOchieng <ogingagabriel@gmail.com>
2024-01-24 18:13:54 +0300

    Update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiSolidChevronDown } from "react-icons/bi";
+import { FaBars, FaTimes } from "react-icons/fa";
+
 import "./navbar.css";
 import clearlink from "../../assets/clearlinkimg.png";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const showNavbar = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="navbar">
       <div>
         <img src={clearlink} alt="" />
       </div>
-      <div>
+      <div className="bigNav">
         <ul>
           <li>
             <h6>Products</h6>
@@ -28,9 +35,44 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-      <div className="buttons">
+      <div className="buttons bigNavbtns">
         <button className="talksl">Talk to sales</button>
         <button className="sign">Sign up for free</button>
+      </div>
+
+      <div onClick={showNavbar} className="nav-btn">
+        {!nav ? (
+          <FaBars className="iconres" />
+        ) : (
+          <div className="nav_open">
+            <div>
+              <FaTimes className="iconres" />
+            </div>
+            <div className="smallNav">
+              <ul>
+                <li>
+                  <h6>Products</h6>
+                  <BiSolidChevronDown />
+                </li>
+                <li>
+                  <h6>Solutions</h6>
+                  <BiSolidChevronDown />
+                </li>
+                <li>
+                  <h6>Resources</h6>
+                  <BiSolidChevronDown />
+                </li>
+                <li>
+                  <h6>Pricing</h6>
+                </li>
+              </ul>
+            </div>
+            <div className="buttons">
+              <button className="talksl">Talk to sales</button>
+              <button className="sign">Sign up for free</button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
